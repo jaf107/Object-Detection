ae56e2cacdde235c2acee825c47d2dd688be7b91
KaziMuktadirAhmed <kazi.muktadir@brainstation-23.com>
2023-10-01 21:39:01 +0600

    it works somehow

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from typing import Union
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File
 import tensorflow_hub as hub
 import tensorflow as tf
 import time
+import cv2
 import uvicorn
+import numpy as np
 
 app = FastAPI()
 
@@ -20,8 +22,23 @@ def load_img(file):
 
 # Define a function to draw boxes on the image
 def draw_boxes(image, boxes, class_entities, scores):
-    # You should implement this function to draw boxes on the image
-    pass
+    # Convert image to NumPy array
+    image_np = image.numpy()
+    
+    for i in range(len(boxes)):
+        box = boxes[i]
+        class_entity = class_entities[i].decode('utf-8')
+        score = scores[i]
+        
+        ymin, xmin, ymax, xmax = box
+        left, right, top, bottom = int(xmin * image_np.shape[1]), int(xmax * image_np.shape[1]), \
+                                   int(ymin * image_np.shape[0]), int(ymax * image_np.shape[0])
+        
+        # Draw bounding box on the image
+        cv2.rectangle(image_np, (left, top), (right, bottom), (0, 255, 0), 2)
+        cv2.putText(image_np, f"{class_entity} ({score:.2f})", (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    
+    return image_np
 
 @app.get("/")
 async def read_root():
@@ -31,13 +48,14 @@ async def read_root():
 async def version():
     return {"1.0"}
 
-
 # Define the endpoint for object detection
-@app.post("/object_detection/")
-async def object_detection(file: UploadFile):
+@app.post("/object_detection")
+async def object_detection(file: UploadFile=File(...)):
+    print(file.filename)
     try:
         # Load the uploaded image
         img = load_img(file.file)
+        print("hello")
         
         # Convert and preprocess the image
         converted_img = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...]
@@ -51,17 +69,15 @@ async def object_detection(file: UploadFile):
         result = {key: value.numpy() for key, value in result.items()}
         
         # Draw boxes on the image
-        # image_with_boxes = draw_boxes(
-        #     img.numpy(), result["detection_boxes"],
-        #     result["detection_class_entities"], result["detection_scores"]
-        # )
+        image_with_boxes = draw_boxes(
+            img, result["detection_boxes"],
+            result["detection_class_entities"], result["detection_scores"]
+        )
+        print("after draw booxes")
         
         # Return the results as JSON
         return {
-            "objects_detected": len(result["detection_scores"]),
-            "objects": result['detection_class_entities'],
-            "inference_time": end_time - start_time
-            # "image_with_boxes": image_with_boxes.tolist()  # Convert to list for JSON compatibility
+            "objects": result['detection_class_entities'].tolist()
         }
     except Exception as e:
         return {"error": str(e)}
